3dc3d92037f7f75550f93b02382650ac78ba7efe
Phil <supersmashphil@gmail.com>
2025-07-29 22:16:50 -0600

    Moved patches to IPatchCategory, added debouncer for update log popup to skip past any last minute load steps from other mods.

---
+++ Source/Patches.cs
@@ -0,0 +1,70 @@
+﻿using HarmonyLib;
+using SmashTools.Patching;
+using SmashTools.Performance;
+using Verse;
+
+namespace UpdateLogTool;
+
+internal class Patches : IPatchCategory
+{
+  private const int DelayUpdateLog = 500; // ms
+
+  // Character Editor stalls for a few seconds on the main menu's first frame. Delay the update log for a small
+  // amount so CE finishes its blocking operation first, otherwise it will pop up before the main menu draws and
+  // then trigger CE to run when the update log is closed, causing people to think the update log is what's freezing
+  // the main menu for a few seconds.
+  private const string CharacterEditor = "void.charactereditor";
+
+  PatchSequence IPatchCategory.PatchAt => PatchSequence.Async;
+
+  void IPatchCategory.PatchMethods()
+  {
+    HarmonyPatcher.Patch(original: AccessTools.Method(typeof(UIRoot_Entry), nameof(UIRoot_Entry.Init)),
+      postfix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(UpdateOnStartup)),
+        priority: Priority.Last));
+    HarmonyPatcher.Patch(
+      original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.FinalizeInit)),
+      postfix: new HarmonyMethod(typeof(Patches),
+        nameof(UpdateOnGameInit)));
+    HarmonyPatcher.Patch(
+      original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.StartedNewGame)),
+      postfix: new HarmonyMethod(typeof(Patches),
+        nameof(UpdateOnNewGame)));
+    HarmonyPatcher.Patch(
+      original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.LoadedGame)),
+      postfix: new HarmonyMethod(typeof(Patches),
+        nameof(UpdateOnLoadedGame)));
+  }
+
+  public static void UpdateOnStartup()
+  {
+    LongEventHandler.ExecuteWhenFinished(delegate
+    {
+      new Debouncer(() => UpdateHandler.CheckUpdates(UpdateFor.Startup), DelayUpdateLog).Invoke();
+    });
+  }
+
+  public static void UpdateOnGameInit()
+  {
+    LongEventHandler.ExecuteWhenFinished(delegate
+    {
+      new Debouncer(() => UpdateHandler.CheckUpdates(UpdateFor.GameInit), DelayUpdateLog).Invoke();
+    });
+  }
+
+  public static void UpdateOnNewGame()
+  {
+    LongEventHandler.ExecuteWhenFinished(delegate
+    {
+      new Debouncer(() => UpdateHandler.CheckUpdates(UpdateFor.NewGame), DelayUpdateLog).Invoke();
+    });
+  }
+
+  public static void UpdateOnLoadedGame()
+  {
+    LongEventHandler.ExecuteWhenFinished(delegate
+    {
+      new Debouncer(() => UpdateHandler.CheckUpdates(UpdateFor.LoadedGame), DelayUpdateLog).Invoke();
+    });
+  }
+}
--- Source/UpdateHandler.cs
+++ Source/UpdateHandler.cs
@@ -1,109 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
-using System.IO;
-using RimWorld;
-using Verse;
-using UnityEngine;
 using HarmonyLib;
+using Verse;
 
-namespace UpdateLogTool
+namespace UpdateLogTool;
+
+[StaticConstructorOnStartup]
+public static class UpdateHandler
 {
-	[StaticConstructorOnStartup]
-	public static class UpdateHandler
-	{
-		public static readonly HashSet<UpdateLog> modUpdates = new HashSet<UpdateLog>();
+  public static readonly HashSet<UpdateLog> modUpdates = [];
 
-		public static readonly HashSet<UpdateLog> updateList = new HashSet<UpdateLog>();
+  public static readonly HashSet<UpdateLog> updateList = [];
 
-		static UpdateHandler()
-		{
-			var harmony = new Harmony("smashphil.updatelog");
-			//Harmony.DEBUG = true;
-			harmony.Patch(original: AccessTools.Method(typeof(UIRoot_Entry), nameof(UIRoot_Entry.Init)),
-				postfix: new HarmonyMethod(typeof(UpdateHandler),
-				nameof(UpdateOnStartup)));
-			harmony.Patch(original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.FinalizeInit)),
-				postfix: new HarmonyMethod(typeof(UpdateHandler),
-				nameof(UpdateOnGameInit)));
-			harmony.Patch(original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.StartedNewGame)),
-				postfix: new HarmonyMethod(typeof(UpdateHandler),
-				nameof(UpdateOnNewGame)));
-			harmony.Patch(original: AccessTools.Method(typeof(GameComponentUtility), nameof(GameComponentUtility.LoadedGame)),
-				postfix: new HarmonyMethod(typeof(UpdateHandler),
-				nameof(UpdateOnLoadedGame)));
+  static UpdateHandler()
+  {
+    foreach (ModContentPack mod in LoadedModManager.RunningModsListForReading)
+    {
+      UpdateLog log = FileReader.LoadUpdateLog(mod);
+      if (log != null)
+      {
+        modUpdates.Add(log);
+      }
+    }
+    SegmentParser.ParseAndCreateSegments();
+    SegmentParser.GenerateRegexText();
+  }
 
-			modUpdates = new HashSet<UpdateLog>();
-			foreach (ModContentPack mod in LoadedModManager.RunningModsListForReading)
-			{
-				UpdateLog log = FileReader.LoadUpdateLog(mod);
-				if (log != null)
-				{
-					modUpdates.Add(log);
-				}
-			}
+  public static UpdateLog UpdateLogData(ModContentPack mod)
+  {
+    return modUpdates.FirstOrDefault(m => m.Mod == mod);
+  }
 
-			SegmentParser.ParseAndCreateSegments();
-			SegmentParser.GenerateRegexText();
-		}
-
-		public static UpdateLog UpdateLogData(ModContentPack mod) => modUpdates.FirstOrDefault(m => m.Mod == mod);
-
-		public static void CheckUpdates(UpdateFor updating)
-		{
-			updateList.Clear();
-			foreach (UpdateLog log in modUpdates)
-			{
-				if (log.UpdateData.updateOn == updating)
-				{
-					if (log.UpdateData.update)
-					{
-						log.NotifyModUpdated();
-						if (!log.UpdateData.description.NullOrEmpty())
-						{
-							updateList.Add(log);
-						}
-					}
-					log.SaveUpdateStatus();
-				}
-			}
-			if (updateList.Any())
-			{
-				Find.WindowStack.Add(new Dialog_NewUpdate(updateList));
-			}
-		}
-
-		public static void UpdateOnStartup()
-		{
-			LongEventHandler.ExecuteWhenFinished(delegate ()
-			{
-				CheckUpdates(UpdateFor.Startup);
-			});
-		}
-
-		public static void UpdateOnGameInit()
-		{
-			LongEventHandler.ExecuteWhenFinished(delegate ()
-			{
-				CheckUpdates(UpdateFor.GameInit);
-			});
-		}
-
-		public static void UpdateOnNewGame()
-		{
-			LongEventHandler.ExecuteWhenFinished(delegate ()
-			{
-				CheckUpdates(UpdateFor.NewGame);
-			});
-		}
-
-		public static void UpdateOnLoadedGame()
-		{
-			LongEventHandler.ExecuteWhenFinished(delegate ()
-			{
-				CheckUpdates(UpdateFor.LoadedGame);
-			});
-		}
-	}
+  public static void CheckUpdates(UpdateFor updating)
+  {
+    updateList.Clear();
+    foreach (UpdateLog log in modUpdates)
+    {
+      if (log.UpdateData.updateOn == updating)
+      {
+        if (log.UpdateData.update)
+        {
+          log.NotifyModUpdated();
+          if (!log.UpdateData.description.NullOrEmpty())
+          {
+            updateList.Add(log);
+          }
+        }
+        log.SaveUpdateStatus();
+      }
+    }
+    if (updateList.Any())
+    {
+      Find.WindowStack.Add(new Dialog_NewUpdate(updateList));
+    }
+  }
 }
